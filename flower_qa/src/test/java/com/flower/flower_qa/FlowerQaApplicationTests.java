11062bc435a6f550e2e94d95d157fd1d2d09a3b3
wangxt59 <wangxt59@163.com>
2019-07-24 10:06:27 +0800

    9

---
+++ flower_qa/src/test/java/com/flower/flower_qa/FlowerQaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flower.flower_qa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlowerQaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
